7b798ff2a2bfee12303c43dc7a70d53dd124c16c
Amy Kang <selene6023@gmail.com>
2014-11-08 22:17:59 -0800

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :check_user, only: [:edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    @posts = Post.all
+    respond_with(@post)
+  end
+
+  def show
+    respond_with(@post)
+  end
+
+  def new
+    @post = Post.new
+    respond_with(@post)
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to(root_url)
+  end
+
+  def update
+    @post.update(post_params)
+    respond_with(@post)
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to(root_url)
+  end
+
+  private
+
+    def check_user
+      unless (@post.user == current_user) || (current_user.admin?)
+        redirect_to root_url, alert: "You are not authorized"
+      end
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:content, :image)
+    end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ActiveRecord::Base
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100" }, :default_url => "default.jpg"
+	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+	default_scope -> {order('created_at DESC')}
+	validates :content, presence: true, length: {maximum: 200}
+	validates_attachment_presence :image
+
+	belongs_to :user
+end
